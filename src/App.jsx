9b99603f12dee206fde720991fb4681cdf3a5b88
Bence Fekete <fekete965@gmail.com>
2023-02-03 09:02:40 +0000

    App.js recommendations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import styles from './style'
+import styles from './style' // If you move your styles as classes into yout App.css, you would not need to import it here
 import {  Navbar,Hero,Footer,AgentCatalouge } from './components'
 import { darkTheme,lightTheme } from './theme'
 import {themeContext} from "./components/themeContext"
@@ -9,10 +9,13 @@ const App = () => {
  
   return (
   <themeContext.Provider value={isDark}>
+    {/* We can have a call how theming can be improved */}
     <div style={isDark?darkTheme:lightTheme} className= "w-full overflow-hidden min-w-[200px]">
+      {/* <div className='my-composed-style'> <--- It would also keep your jsx nice and clean */}
       <div className={` ${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar/>
+          {/* If you are not using the "event", you don't need to define it: () => setDark(prev => !prev) */}
           <button style={{width:"50px "}} onClick={(event)=>setDark(prev=>!prev)}>Dark</button>
         </div>
       </div>
